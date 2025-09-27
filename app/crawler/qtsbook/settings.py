bf8f8e4d78e10b3f2e6351e025703fa0ca2327d5
Tohfa Akib <tnishan7@gmail.com>
2025-09-27 13:22:40 +0600

    enabled pipeline

--- app/crawler/qtsbook/settings.py
+++ app/crawler/qtsbook/settings.py
@@ -74,7 +74,7 @@ DEFAULT_REQUEST_HEADERS = {
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   # "qtsbook.pipelines.MongoPipeline": 300,
+   "qtsbook.pipelines.MongoPipeline": 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
